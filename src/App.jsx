ec7a59d754a1523069a4e5c7884c2e59709475ef
Barbara Tess <barbaratessn@gmail.com>
2024-03-19 02:28:30 +0300

    Home page configuration
    Application launched using Vite.
    
    Unnecessary files removed from the Vite default.
    
    Addition of a simple header component that can be edited in time.
    
    Addition of a home page route with very simple styling just to get the project going.
    
    Installation of react-router-dom to handle routing.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+
+function App() {
+    return (
+        <>
+            <Router>
+                <div className='container'>
+                    <Header />
+                    <Routes>
+                        <Route
+                            path='/'
+                            element={<Home />}
+                        />
+                    </Routes>
+                </div>
+            </Router>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            <h1>stocktaking-app</h1>
+        </div>
+    )
+}
+
+export default Header
