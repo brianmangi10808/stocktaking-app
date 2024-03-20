c9fc7ad131e5d0824f6b0bd3c9f296d0296dad9d
Sylviachelimo <chelimosylvia280@gmail.com>
2024-03-20 15:49:57 +0300

    Loginpage configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
+import LoginPage from './pages/LoginPage'
 
 function App() {
     return (
         <>
             <Router>
                 <div className='container'>
+                    <LoginPage/>
                     <Header />
                     <Routes>
                         <Route
