c9f72b94b88d56c51b67c3cf9c3d4abb9258140c
collinsbett023 <collinsbett023@proton.me>
2024-03-23 23:43:34 +0300

    Completed admin page

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,42 @@ import LoginPage from './pages/LoginPage'
 import NextPage from './pages/NextPage'
 import ForgotPassword from './pages/ForgotPassword'
 import Signup from './pages/Signup'
+import Admin from './pages/Admin.jsx'
+import Edit from './components/Admin/Edit.jsx'
+import { useEffect, useState } from 'react'
 
 function App() {
+    const [products, setProducts] = useState([])
+
+   
+
+    const url = `http://localhost:3000/clothes`
+    
+  
+    useEffect(() => {
+     
+      fetch(`http://localhost:3000/clothes`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data)
+  
+      })
+    },[])
+  
+    //DELETE Method
+    const deleteClothes = (id) => {
+      fetch(url+`/${id}`,{
+        method: "DELETE"
+      })
+      .then((res) => !res.ok ? console.log("Problem") : res.json() )
+      window.location.reload()
+  }
     return (
+            <>
+            <Header />
             <Router>
-                <div className='container'>
-                    <Header />
+               
+                    
                     <Routes>
                         <Route path='/' element={<Home />} />
                         <Route path='/signup' element={<Signup />} />
@@ -18,9 +48,19 @@ function App() {
 
                         <Route path='/NextPage' element={<NextPage />} />
                         <Route path='/ForgotPassword' element={<ForgotPassword />} />
+
+                        <Route
+                          path='/admin'
+                          element={<Admin clothes={products} deleteCloth={deleteClothes}/>}
+                      />
+              <Route
+                path='/edit/:id'
+                element={<Edit/>}
+                />
                     </Routes>
-                </div>
+                
             </Router>
+            </>
     )
 }
 
